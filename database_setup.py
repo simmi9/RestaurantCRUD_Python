fe88d975a90ba4773e1dae8ed40bc9f07bee3fbf
vagrant <vagrant@vagrant.vm>
2018-09-16 02:23:29 +0000

    created myfirstRestaurant and added cheesepizza to its menuItems

--- database_setup.py
+++ database_setup.py
@@ -30,3 +30,11 @@ Base.metadata.create_all(engine)
 
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
+
+myFirstRestaurant = Restaurant(name = "Pizza Palace")
+session.add(myFirstRestaurant)
+sesssion.commit()
+
+cheesepizza = menuItem(name="Cheese Pizza", description = "Made with all natural ingredients and fresh mozzarella", course="Entree", price="$8.99", restaurant=myFirstRestaurant)
+session.add(cheesepizza)
+session.commit()
