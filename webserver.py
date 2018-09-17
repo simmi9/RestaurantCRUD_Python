d34015efe807d33bdb0951fb0ee545bb8689f4db
vagrant <vagrant@vagrant.vm>
2018-09-17 16:00:43 +0000

    Defined main method to instantiate server and to specify its port

--- webserver.py
+++ webserver.py
@@ -1,1 +1,13 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+def main():
+	try:
+		port = 80
+		server = HTTPServer(('', port), webServerHandler)
+		print "Web Server running on port %s"  % port
+		server.serve_forever()
+	except KeyboardInterrupt:
+		print " ^C entered, stopping web server...."
+		server.socket.close()
+
+if __name__ == '__main__':
+	main()
