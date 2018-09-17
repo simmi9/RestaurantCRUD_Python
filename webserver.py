aea6048d4651716a7b479d14dcd32f26d34a0871
vagrant <vagrant@vagrant.vm>
2018-09-17 19:23:40 +0000

    Added definition for handling POST form data request using cgi

--- webserver.py
+++ webserver.py
@@ -1,4 +1,6 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
+
 
 class webServerHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
@@ -29,7 +31,28 @@ class webServerHandler(BaseHTTPRequestHandler):
                 return
 
         except IOError:
-            self.send_error(404, 'File Not Found: %s' % self.path)		
+            self.send_error(404, 'File Not Found: %s' % self.path)	
+
+    def do_POST(self):
+        try:
+            self.send_response(301)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            ctype, pdict = cgi.parse_header(
+                self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('message')
+            output = ""
+            output += "<html><body>"
+            output += " <h2> Okay, how about this: </h2>"
+            output += "<h1> %s </h1>" % messagecontent[0]
+            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+            output += "</body></html>"
+            self.wfile.write(output)
+            print output
+        except:
+            pass        
 
 def main():
 	try:
