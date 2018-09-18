86061dcf27bfa7b26f244f81c13dcf39cdb815da
vagrant <vagrant@vagrant.vm>
2018-09-18 16:50:31 +0000

    Adding CRUD operations library from lotsofmenus.py

--- webserver.py
+++ webserver.py
@@ -1,23 +1,29 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
+#importing CRUD Operations
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
 
 
 class webServerHandler(BaseHTTPRequestHandler):
-	def do_GET(self):
-		try:
-			if self.path.endswith("/hello"):
-				self.send_response(200)
-				self.send_header('Content-type', 'text/html')
-				self.end_headers()
-				message = ""
-				message += "<html><body>Hello!</body></html>"
-				self.wfile.write(message)
-				print message
-				return
-		#except IOError:
-		#	self.send_error(404, 'File Not Found: %s' % self.path)
 
-	        if self.path.endswith("/hola"):
+    def do_GET(self):
+        try:
+            if self.path.endswith("/hello"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                output += "<h1>Hello!</h1>"
+                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
+                return
+
+            if self.path.endswith("/hola"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
@@ -30,8 +36,22 @@ class webServerHandler(BaseHTTPRequestHandler):
                 print output
                 return
 
+            if self.path.endswith("/restaurants"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                output += "<h1>Restaurant Names</h1>"
+                output += '''<div class="container-fluid text-center"> %s  </div>''' 
+                #output += '''<form method='POST' enctype='multipart/form-data' action='/restaurant'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
+                return    
+
         except IOError:
-            self.send_error(404, 'File Not Found: %s' % self.path)	
+            self.send_error(404, 'File Not Found: %s' % self.path)
 
     def do_POST(self):
         try:
@@ -52,17 +72,18 @@ class webServerHandler(BaseHTTPRequestHandler):
             self.wfile.write(output)
             print output
         except:
-            pass        
+            pass
+
 
 def main():
-	try:
-		port = 8080
-		server = HTTPServer(('', port), webServerHandler)
-		print "Web Server running on port %s"  % port
-		server.serve_forever()
-	except KeyboardInterrupt:
-		print " ^C entered, stopping web server...."
-		server.socket.close()
+    try:
+        port = 8080
+        server = HTTPServer(('', port), webServerHandler)
+        print "Web Server running on port %s" % port
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print " ^C entered, stopping web server...."
+        server.socket.close()
 
 if __name__ == '__main__':
-	main()
+    main()
