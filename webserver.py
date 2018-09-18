f87a6ac808c01b84a6561776820d2095ac82ad5a
vagrant <vagrant@vagrant.vm>
2018-09-18 19:38:01 +0000

    Added code for renaming restaurant using do_POST

--- webserver.py
+++ webserver.py
@@ -119,11 +119,65 @@ class webServerHandler(BaseHTTPRequestHandler):
                 self.wfile.write(output)
                 return
 
+            if self.path.endswith("/edit"):
+                restaurantIDPath = self.path.split("/")[2] #finding id of restaurant name in /restaurants
+                myRestaurantQuery = session.query(Restaurant).filter_by(
+                    id=restaurantIDPath).one()
+                if myRestaurantQuery:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    output += '''<!DOCTYPE html><html lang="en"><head>
+                    <title>Bootstrap Example</title>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+                    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+                    </head>'''
+                    output += "<body>"
+                    output += "<h1>Edit Restaurant</h1>"
+                    output += "<h2>"
+                    output += myRestaurantQuery.name
+                    output += "</h2>"
+                    output += '''<form method ='POST'  enctype='multipart/form-data' action = '/restaurants/%s/edit'>''' % restaurantIDPath
+                    output +='''<div class="form-group">'''
+                    output += '''<label for="Rename Restaurant" >Rename Restaurant"</label>"'''
+                    output += '''<input type="text" class="form-control" id="rname" name = 'newRestaurantName' 
+                    placeholder = '%s'>''' % myRestaurantQuery.name
+                    output += "</label>"
+                    output +='''</div>'''
+                    output += "<input type='submit' value='Rename'>"
+                    output += "</form></body></html>"
+                    self.wfile.write(output)
+                    return
+
+
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
 
     def do_POST(self):
         try:
+            if self.path.endswith("/edit"):
+                ctype, pdict = cgi.parse_header(
+                    self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+                    restaurantIDPath = self.path.split("/")[2]
+
+                    myRestaurantQuery = session.query(Restaurant).filter_by(
+                        id=restaurantIDPath).one()
+                    if myRestaurantQuery != []:
+                        myRestaurantQuery.name = messagecontent[0]
+                        session.add(myRestaurantQuery)
+                        session.commit()
+                        self.send_response(301)
+                        self.send_header('Content-type', 'text/html')
+                        self.send_header('Location', '/restaurants')
+                        self.end_headers()
+                        
             if self.path.endswith("/restaurants/new"):
                 ctype, pdict = cgi.parse_header(
                     self.headers.getheader('content-type'))
