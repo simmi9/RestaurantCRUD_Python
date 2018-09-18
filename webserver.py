24e3d6888f9508453aaf667b4b418c9fcf5beccc
vagrant <vagrant@vagrant.vm>
2018-09-18 16:51:57 +0000

    Adding create session and bind database command from lotsofmenus.py

--- webserver.py
+++ webserver.py
@@ -6,6 +6,11 @@ from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
 
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind=engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
 class webServerHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
