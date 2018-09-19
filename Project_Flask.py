76ad8bdf75e9fec5b10faef4f1e9c1fa57d5fa7d
vagrant <vagrant@vagrant.vm>
2018-09-19 18:30:36 +0000

    Created Routing template for New, Edit and Delete Menu Items

--- Project_Flask.py
+++ Project_Flask.py
@@ -12,13 +12,11 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
-
-
 #@app.route('/')
-@app.route('/restaurants/<int:restaurant_id>/')
 #def hello():
+@app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
-    restaurant = session.query(Restaurant).first()
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
     output = ''
     for i in items:
@@ -30,6 +28,18 @@ def restaurantMenu(restaurant_id):
         output += '</br>'
         output += '</br>'
     return output
+
+Task 1: Create route for newMenuItem function here
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+# Task 2: Create route for editMenuItem function here
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+# Task 3: Create a route for deleteMenuItem function here
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"    
 
 
 if __name__ == '__main__':
