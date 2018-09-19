6f3585b1967c2e05f3b6db18cf1958020a43ca52
vagrant <vagrant@vagrant.vm>
2018-09-19 16:10:42 +0000

    Integrated Flask code with sqlalchemy RestaurantMenuItem CRUD

--- Project_Flask.py
+++ Project_Flask.py
@@ -1,12 +1,41 @@
 from flask import Flask
 app = Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    return "Hello World"
+def hello():
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+        output += i.price
+        output += '</br>'
+    return output
+
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host = '0.0.0.0', port = 5000)
+
+#@app.route('/')
+#@app.route('/hello')
+#def HelloWorld():
+#    return "Hello World"
+
+#if __name__ == '__main__':
+#    app.debug = True
+#    app.run(host='0.0.0.0', port=5000)
